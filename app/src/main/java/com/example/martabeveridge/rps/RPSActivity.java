71bc78230b524d25946a5c13edcf153614f0fc30
martabeveridge <marta@beveridge.uk>
2018-04-24 13:39:38 +0100

    Created link between element IDs and Java file

--- app/src/main/java/com/example/martabeveridge/rps/RPSActivity.java
+++ app/src/main/java/com/example/martabeveridge/rps/RPSActivity.java
@@ -2,12 +2,28 @@ package com.example.martabeveridge.rps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class RPSActivity extends AppCompatActivity {
+
+    EditText playerName;
+    TextView resultText;
+    ImageButton rockButton;
+    ImageButton paperButton;
+    ImageButton scissorsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rps);
+
+        playerName = findViewById(R.id.playerNameID);
+        resultText = findViewById(R.id.resultTextID);
+        rockButton = findViewById(R.id.rockButtonID);
+        paperButton = findViewById(R.id.paperButtonID);
+        scissorsButton = findViewById(R.id.scissorButtonID);
+
     }
 }
